86245a3a52fa19941916a0158dc29b672ffaff48
hectorfcbbcf@gmail.com <Hectorgs1988>
2023-06-08 19:03:57 +0200

    V2

--- GarciaSanchezHector_PEC_FINAL/js/products.js
+++ GarciaSanchezHector_PEC_FINAL/js/products.js
@@ -1,35 +1,35 @@
 let listaCompra = [];
 
 class Producto {
-    constructor(nombre, tipo, enlace){
+    constructor(nombre, tipo, enlace) {
         this.nombre = nombre;
         this.tipo = tipo;
         this.enlace = enlace;
     }
-    
+
     // Métodos getter
     getNombre() {
         return this.nombre;
     }
 
-    getTipo(){
+    getTipo() {
         return this.tipo;
     }
 
-    getEnlace(){
+    getEnlace() {
         return this.enlace;
     }
 
     // Metodos setter
-    setNombre(nombre){
+    setNombre(nombre) {
         this.nombre = nombre;
     }
 
-    setTipo(tipo){
+    setTipo(tipo) {
         this.tipo = tipo;
     }
 
-    setEnlace(enlace){
+    setEnlace(enlace) {
         this.enlace = enlace;
     }
 }
@@ -66,11 +66,9 @@ let productos = [
     new Producto("Pasta fresca", "Cereales y pastas", "img/pastaFresca.jpg"),
     new Producto("Avena", "Cereales y pastas", "img/avena.jpg"),
     new Producto("Quinoa", "Cereales y pastas", "img/quinoa.jpg"),
-
-
 ];
 
-// Obtén una referencia al div de categorías
+// Obtiene una referencia al div de categorías
 let divCategorias = document.getElementById("categorias");
 
 // Crea un array para almacenar las categorías de productos únicas
@@ -87,7 +85,7 @@ productos.forEach(producto => {
 // Crea una variable para almacenar la última categoría que se mostró
 let ultimaCategoriaMostrada = null;
 
-// Crea un botón para cada categoría y añádelo al div de categorías
+// Crea un botón para cada categoría y lo añádelo al div de categorías
 categorias.forEach(categoria => {
     let divCategoria = document.createElement("div");
     divCategoria.className = "categoria";
@@ -98,7 +96,7 @@ categorias.forEach(categoria => {
     let divProductosCategoria = document.createElement("div");
     divProductosCategoria.className = "productosCategoria";
 
-    botonCategoria.addEventListener("click", function() {
+    botonCategoria.addEventListener("click", function () {
         if (categoria === ultimaCategoriaMostrada) {
             // Si la categoría que se hizo clic es la misma que la última que se mostró, limpia el div de los productos y establece la última categoría mostrada a null
             divProductosCategoria.innerHTML = "";
@@ -115,6 +113,7 @@ categorias.forEach(categoria => {
     divCategorias.appendChild(divCategoria);
 });
 
+//Muestra los productos de una categoria, permite hacer clic para añadir cantidad y agregarlo a la lista
 function mostrarProductos(categoria, divProductosCategoria) {
     divProductosCategoria.innerHTML = "";
 
@@ -129,33 +128,32 @@ function mostrarProductos(categoria, divProductosCategoria) {
         let pNombre = document.createElement('p');
         pNombre.textContent = producto.getNombre();
 
-        imgProducto.addEventListener("click", function() {
+        imgProducto.addEventListener("click", function () {
             let cantidad = prompt("¿Cuántas unidades de este producto quieres?", "1");
-        
+
             if (cantidad !== null) {
                 let productoCompra = { producto: producto.getNombre(), cantidad: cantidad };
                 listaCompra.push(productoCompra);
                 localStorage.setItem('listaCompra', JSON.stringify(listaCompra));
-        
+
                 // Crear el elemento de mensaje y añadirlo al body
                 let message = document.createElement('div');
                 message.id = 'message';
                 message.textContent = 'Producto añadido correctamente';
                 document.body.appendChild(message);
-        
+
                 // Mostrar el mensaje y luego ocultarlo después de 2 segundos
                 message.classList.add('show');
-                setTimeout(function() {
+                setTimeout(function () {
                     message.classList.remove('show');
-        
-                    // Remove the message element after it has faded out
-                    setTimeout(function() {
+
+                    // Borrar el mensaje
+                    setTimeout(function () {
                         document.body.removeChild(message);
                     }, 500);
                 }, 2000);
             }
         });
-        
 
         divProducto.appendChild(pNombre);
         divProducto.appendChild(imgProducto);
@@ -165,73 +163,72 @@ function mostrarProductos(categoria, divProductosCategoria) {
 
 
 
+// Desde aqui controlo el guardado, la visualización de la ultima lista, visualización de todas las listas guardadas en el localStorage
+document.addEventListener("DOMContentLoaded", function () {
 
-
-
-
-// Asegúrate de que tu DOM está completamente cargado antes de intentar seleccionar elementos o añadir manejadores de eventos
-document.addEventListener("DOMContentLoaded", function() {
-
-    document.getElementById("guardar").addEventListener("click", function() {
+    document.getElementById("guardar").addEventListener("click", function () {
         let now = new Date();
         let formattedDate = now.toISOString();
-        let listaCompraConFecha = {fecha: formattedDate, productos: listaCompra};
+        let listaCompraConFecha = { fecha: formattedDate, productos: listaCompra };
         localStorage.setItem(formattedDate, JSON.stringify(listaCompraConFecha));
         listaCompra = [];
         console.log('Lista de la compra guardada');
-    
+
         // Crear el elemento de mensaje y añadirlo al body
         let message = document.createElement('div');
         message.id = 'message';
         message.textContent = 'Lista guardada correctamente';
         document.body.appendChild(message);
-    
+
         // Mostrar el mensaje y luego ocultarlo después de 2 segundos
         message.classList.add('show');
-        setTimeout(function() {
+        setTimeout(function () {
             message.classList.remove('show');
-    
+
             // Eliminar el elemento de mensaje después de que se haya desvanecido
-            setTimeout(function() {
+            setTimeout(function () {
                 document.body.removeChild(message);
             }, 500);
         }, 2000);
     });
-    
-    
 
 
-    document.getElementById("mostrar").addEventListener("click", function() {
+
+
+    document.getElementById("mostrar").addEventListener("click", function () {
         // Obtener todas las claves de localStorage
         let keys = Object.keys(localStorage);
-    
+
         // Filtrar solo las claves que tienen formato de fecha ISO
         let listKeys = keys.filter(key => {
             return /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(key);
         });
-    
+
         // Si no hay ninguna lista guardada
         if (listKeys.length === 0) {
             console.log('No hay ninguna lista guardada.');
             return;
         }
-    
+
         // Ordenar las claves en orden descendente
         listKeys.sort().reverse();
-    
+
         // Obtener la última lista de la compra
         let ultimaListaCompra = JSON.parse(localStorage.getItem(listKeys[0]));
-    
+
         console.log(ultimaListaCompra);
     });
 
-    
 
-    document.getElementById("listasBoton").addEventListener("click", function() {
+
+    document.getElementById("listasBoton").addEventListener("click", function () {
         let divListas = document.getElementById('displayListas');
-        divListas.innerHTML = "";  // Limpia el div antes de agregar nuevas listas
 
-        let listas = Object.keys(localStorage).sort().reverse(); // Ordena las claves (fechas) en orden descendente
+        // Limpia el div antes de agregar nuevas listas
+        divListas.innerHTML = "";
+
+        // Ordena las claves (fechas) en orden descendente
+        let listas = Object.keys(localStorage).sort().reverse();
 
         listas.forEach(lista => {
             let listaCompra = JSON.parse(localStorage.getItem(lista));
@@ -240,16 +237,17 @@ document.addEventListener("DOMContentLoaded", function() {
             if (listaCompra && listaCompra.productos) {
                 let pFecha = document.createElement('p');
                 let fecha = new Date(listaCompra.fecha);
-                pFecha.textContent = `Fecha: ${fecha.getFullYear()}-${(fecha.getMonth()+1).toString().padStart(2, '0')}-${fecha.getDate().toString().padStart(2, '0')} ${fecha.getHours().toString().padStart(2, '0')}:${fecha.getMinutes().toString().padStart(2, '0')}`;
+                pFecha.textContent = `Fecha: ${fecha.getFullYear()}-${(fecha.getMonth() + 1).toString().padStart(2, '0')}-${fecha.getDate().toString().padStart(2, '0')} ${fecha.getHours().toString().padStart(2, '0')}:${fecha.getMinutes().toString().padStart(2, '0')}`;
                 divListas.appendChild(pFecha);
 
                 listaCompra.productos.forEach(item => {
                     let pItem = document.createElement('p');
-                    pItem.textContent = `Producto: ${item.producto}, Cantidad: ${item.cantidad}`; // Corregido item[0] y item[1] por item.producto y item.cantidad
+                    pItem.textContent = `Producto: ${item.producto}, Cantidad: ${item.cantidad}`;
                     divListas.appendChild(pItem);
                 });
 
-                let separator = document.createElement('hr'); // Agrega una línea horizontal entre las listas
+                //Linea horizontal
+                let separator = document.createElement('hr');
                 divListas.appendChild(separator);
             } else {
 
--- GarciaSanchezHector_PEC_FINAL/js/register.js
+++ GarciaSanchezHector_PEC_FINAL/js/register.js
@@ -1,13 +1,20 @@
-//VER QUE HACER CON ESTO
-// Poblaciones y códigos postales
+
+//Poblaciones y códigos postales
 var poblacionesCodigosPostales = {
+    "Seleccione una población": "",
     "Burgos": "09007",
     "Soria": "09001",
-    "Barcelona": "09002"
-    // etc.
+    "Barcelona": "09002",
+    "Vitoria": "09003",
+    "Zaragoza": "09004",
+    "Susinos del Paramo": "09133",
+    "Sevilla": "09006",
+    "Malaga": "09010",
+    "Caceres": "09008",
+    "Teruel": "09009",
 };
 
-// Rellenar el select de poblaciones
+//Rellenar el select de poblaciones
 var selectPoblacion = document.getElementById('poblacion');
 for (var poblacion in poblacionesCodigosPostales) {
     var option = document.createElement('option');
@@ -16,11 +23,12 @@ for (var poblacion in poblacionesCodigosPostales) {
     selectPoblacion.add(option);
 }
 
-// Asumiendo que tienes un elemento de entrada para el código postal con el id 'codigoPostal'
+// Guardo "codigoPostal" en la variable inputCodigoPostal
 var inputCodigoPostal = document.getElementById('codigoPostal');
 
 // Controlador de eventos para cuando se cambia la selección en el select de poblaciones
-selectPoblacion.addEventListener('change', function() {
+selectPoblacion.addEventListener('change', function () {
+
     // Buscar el código postal para la población seleccionada
     var codigoPostal = poblacionesCodigosPostales[this.value];
 
@@ -29,8 +37,8 @@ selectPoblacion.addEventListener('change', function() {
 });
 
 
-
-document.getElementById("registerButton").addEventListener("click", function(event){
+//Recoger los valores introducidos por el usuario en el formulario de registro
+document.getElementById("registerButton").addEventListener("click", function (event) {
     event.preventDefault();
 
     let u = document.getElementById("Usuario").value;
@@ -42,53 +50,65 @@ document.getElementById("registerButton").addEventListener("click", function(eve
     let codigoPostal = document.getElementById("codigoPostal").value;
     let telefono = document.getElementById("telefono").value;
     let correoElectronico = document.getElementById("correoElectronico").value;
-    // let usuario = document.getElementById('username').value;
+
 
 
     // Crea un objeto de usuario con los datos ingresados
-  let user = new Usuario(nombre, apellidos, direccion, poblacion, codigoPostal, telefono, correoElectronico, u, contraseña);
+    let user = new Usuario(nombre, apellidos, direccion, poblacion, codigoPostal, telefono, correoElectronico, u, contraseña);
 
-  // Guarda el objeto de usuario en el localStorage
-  // Los objetos deben ser convertidos a formato JSON para poder ser guardados en el localStorage
-  localStorage.setItem(u, JSON.stringify(user));
+    // Guarda el objeto de usuario en el localStorage
+    // Los objetos deben ser convertidos a formato JSON para poder ser guardados en el localStorage
+    localStorage.setItem(u, JSON.stringify(user));
 
 
 
-    // Validaciones
+    // Validaciones de los campos de el formulario de registro
+
+    // Campos que son obligatorios, si alguno de ellos esta vacio se muestra un mensaje.
     if (nombre === '' || apellidos === '' || direccion === '' || u === '') {
         alert('Nombre, Apellidos, Dirección y Usuario son campos obligatorios');
         return;
     }
 
+    //Comprobar que la poblacion coincide con su codigo postal (CP simulados, no son reales)
     if (poblacionesCodigosPostales[poblacion] !== codigoPostal) {
         alert('La población y el código postal no coinciden.');
         return;
     }
 
+    //Expresion regular que comprueba si el formato del numero de telefono es correcto.
+    //Estos son los formatos soportados: 
+    // +1-800-123-4567
+    // (1) 800 123 4567
+    // 18001234567
+    // 800.123.4567
     if (!/^[\+]?[(]?[0-9]{1,4}[)]?[-\s\.]?[0-9]{1,4}[-\s\.]?[0-9]{4,6}$/.test(telefono)) {
         alert('El teléfono proporcionado no es válido.');
         return;
     }
 
+    //Expresion regular que comprueba si el formato del email introducido es correcto
     if (!/\S+@\S+\.\S+/.test(correoElectronico)) {
         alert('El correo electrónico proporcionado no es válido.');
         return;
     }
 
-   
-
+    //Comprobar si el usuario ya existe en el localStorage
     let usuariosExistentes = Usuario.deserialize();
 
-if (usuariosExistentes.some(user => user.usuario === u)) {
-    alert('El nombre de usuario ya está en uso.');
-    return;
-}
+    if (usuariosExistentes.some(user => user.usuario === u)) {
+        alert('El nombre de usuario ya está en uso.');
+        return;
+    }
 
-usuariosExistentes.push(user); // Agrega el nuevo usuario a la lista
-Usuario.serialize(usuariosExistentes); // Guarda la lista actualizada en localStorage
+    // Agrega el nuevo usuario a la lista
+    usuariosExistentes.push(user);
 
-    
+    // Guarda la lista actualizada en localStorage
+    Usuario.serialize(usuariosExistentes);
 
+
+    //Expresion regular que comprueba si la contraseña introducida cumple con las condiciones
     if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(contraseña)) {
         alert('La contraseña debe contener mínimo 8 caracteres, letras, números y al menos dos caracteres especiales.');
         return;
